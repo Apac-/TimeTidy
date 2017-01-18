73b39156627150e01cc0b8cd3bf1a14b063b9499
Apac- <silicon.apac@gmail.com>
2017-01-18 13:38:01 +1000

    Initial commit

---
+++ Vigilance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vigilance.Startup))]
+namespace Vigilance
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
