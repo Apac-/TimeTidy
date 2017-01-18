987cd444791e1483ad4ee7124487bc13e6ee89af
Apac <jeff@templarit.com.au>
2017-01-18 14:11:52 +1000

    AddAnnotationstoWorkSite

---
+++ Vigilance/Migrations/201701180411051_ApplyRequirementsToWorkSite.cs
@@ -0,0 +1,18 @@
+namespace Vigilance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyRequirementsToWorkSite : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.WorkSites", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.WorkSites", "Name", c => c.String());
+        }
+    }
+}
--- Vigilance/Models/WorkSite.cs
+++ Vigilance/Models/WorkSite.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vigilance.Models {
     public class WorkSite {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [Required]
         public float Lat { get; set; }
+        [Required]
         public float Lng { get; set; }
+
         public int Radius { get; set; }
     }
 }
