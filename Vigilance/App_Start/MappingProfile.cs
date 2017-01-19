213a1e45508cc2e3d7ab84bd5bae80f29015d77a
Apac <jeff@templarit.com.au>
2017-01-19 14:16:50 +1000

    AddMappingProfileForWorkSiteDTO

---
+++ Vigilance/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vigilance.DTOs;
+using Vigilance.Models;
+
+namespace Vigilance.App_Start {
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<WorkSite, WorkSiteDTO>();
+            Mapper.CreateMap<WorkSiteDTO, WorkSite>();
+        }
+    }
+}
