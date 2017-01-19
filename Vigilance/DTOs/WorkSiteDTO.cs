6c4d4da2f70aff4d9cf0045b7bd6d0ce31586d03
Apac <jeff@templarit.com.au>
2017-01-19 14:08:14 +1000

    CreateWorkSiteDTO

---
+++ Vigilance/DTOs/WorkSiteDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.DTOs {
+    public class WorkSiteDTO {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public float Lat { get; set; }
+        [Required]
+        public float Lng { get; set; }
+
+        public int Radius { get; set; }
+    }
+}
