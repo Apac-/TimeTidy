6bd2b7140227ac4cce3c94f906069e30a6f0e7da
Apac <jeff@templarit.com.au>
2017-01-20 18:38:15 +1000

    Changed Bundle jquery to lib. Now will house 3rd party scripts

--- Vigilance/App_Start/BundleConfig.cs
+++ Vigilance/App_Start/BundleConfig.cs
@@ -4,9 +4,12 @@ using System.Web.Optimization;
 namespace Vigilance {
     public class BundleConfig {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/datatables/jquery.datatables.js",
+                "~/Scripts/datatables/datatables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
