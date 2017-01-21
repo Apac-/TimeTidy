8ae6f111a5b1be36bce87e96f3ee778080ce51da
Apac <jeff@templarit.com.au>
2017-01-21 15:12:24 +1000

    Add RoleName const strings

---
+++ Vigilance/Models/RoleName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.Models {
+    public class RoleName
+    {
+        public const string CanManageWorkSites = "CanManageWorkSites";
+    }
+}
