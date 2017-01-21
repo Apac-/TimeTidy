fd2d436dcc2e21ed79c9aa1c876030690daa4e21
Apac <jeff@templarit.com.au>
2017-01-21 19:55:57 +1000

    Add private context, userManager and roleManager

--- Vigilance/Controllers/UsersController.cs
+++ Vigilance/Controllers/UsersController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Vigilance.Models;
 
 namespace Vigilance.Controllers
 {
     public class UsersController : Controller
     {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public UsersController()
+        {
+            _context = new ApplicationDbContext();
+            _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+        }
+
         // GET: Users
         public ActionResult Index()
         {
-            return View();
+            var users = _userManager.Users.ToList();
+
+            return View("Index", users);
         }
     }
 }
