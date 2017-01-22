ca032e1f2237c02bdd6585e5f173df41ef417307
Apac <jeff@templarit.com.au>
2017-01-22 13:51:49 +1000

    Create UserDTO

---
+++ Vigilance/Models/DTOs/UsersDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.Models.DTOs {
+    public class UsersDTO {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
