c02f812dfcf557dfd6e34dcea62e376d3a84e3ab
Apac <jeff@templarit.com.au>
2017-01-22 22:55:41 +1000

    Init UserIdViewModel

---
+++ Vigilance/Models/UserIDViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.Models {
+    public class UserIdViewModel {
+        public string Id { get; set; }
+    }
+}
