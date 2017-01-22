2bc5403bbecb039a7c850f7a08a23571e198970c
Apac <jeff@templarit.com.au>
2017-01-22 15:34:36 +1000

    Init LatLng

---
+++ Vigilance/Models/LatLng.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.Models {
+    public class LatLng {
+        public float? Lat { get; set; }
+        public float? Lng { get; set; }
+    }
+}
