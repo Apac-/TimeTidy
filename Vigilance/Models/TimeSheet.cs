9e0069a50ff90d5f37a85b19007d3c508e02ee3b
Apac <jeff@templarit.com.au>
2017-01-22 15:20:36 +1000

    Fillout timesheet

--- Vigilance/Models/TimeSheet.cs
+++ Vigilance/Models/TimeSheet.cs
@@ -1,9 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vigilance.Models {
     public class TimeSheet {
+        public int Id { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public int ApplicationUserId { get; set; }
+
+        [Required]
+        public WorkSite WorkSite { get; set; }
+
+        [Required]
+        public DateTime LogOnTime { get; set; }
+
+        [Required]
+        public Tuple<float,float> LogOnLatLng { get; set; }
+
+        public DateTime? LogOffTime { get; set; }
+        public Tuple<float,float> LogOffLatLng { get; set; }
     }
 }
