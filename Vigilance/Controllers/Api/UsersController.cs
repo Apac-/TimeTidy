09b28e023d2ee710bc20cb7cb2080cfd6ff0beb3
Apac <jeff@templarit.com.au>
2017-01-22 13:45:06 +1000

    Init Users API controller

---
+++ Vigilance/Controllers/Api/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Vigilance.Models;
+
+namespace Vigilance.Controllers.Api
+{
+    public class UsersController : ApiController
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public UsersController()
+        {
+            _context = new ApplicationDbContext();
+            _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+        }
+
+        public IHttpActionResult GetUsers()
+        {
+            
+        }
+    }
+}
