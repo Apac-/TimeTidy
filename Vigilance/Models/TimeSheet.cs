c3f87b9b37e5c92a91cf1edf889661170874a1ed
Apac <jeff@templarit.com.au>
2017-01-23 19:09:12 +1000

    Set up TimeSheet for Db

--- Vigilance/Models/TimeSheet.cs
+++ Vigilance/Models/TimeSheet.cs
@@ -8,8 +8,11 @@ namespace Vigilance.Models {
     public class TimeSheet {
         public int Id { get; set; }
 
+        public virtual ApplicationUser ApplicationUser{ get; set; }
+
         [Required]
         public string ApplicationUserId { get; set; }
+
 
         [Required]
         public WorkSite WorkSite { get; set; }
