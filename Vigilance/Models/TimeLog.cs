2e95ebf73495c8d3faae092b5966860f57724355
Apac <jeff@templarit.com.au>
2017-01-23 20:32:20 +1000

    Add Id

--- Vigilance/Models/TimeLog.cs
+++ Vigilance/Models/TimeLog.cs
@@ -5,6 +5,8 @@ using System.Web;
 
 namespace Vigilance.Models {
     public class TimeLog {
+        public int Id { get; set; }
+
         public DateTime Time { get; set; }
         public LatLng Location { get; set; }
     }
