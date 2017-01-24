44a42c5c682f42b8ea85a36c078e421989136ea3
Apac <jeff@templarit.com.au>
2017-01-24 16:54:28 +1000

    Add timeSheetID

---
+++ Vigilance/Models/DTOs/LogOnTimeDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.Models.DTOs {
+    public class LogOnTimeDTO {
+        public DateTime? DateTime { get; set; }
+        public int? TimeSheetId { get; set; }
+    }
+}
