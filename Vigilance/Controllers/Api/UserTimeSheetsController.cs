c7d06f020cd015860e0e8d71e13c230dafdcd5bc
Apac <jeff@templarit.com.au>
2017-01-24 22:16:03 +1000

    Include loading of LatLngs from db

--- Vigilance/Controllers/Api/UserTimeSheetsController.cs
+++ Vigilance/Controllers/Api/UserTimeSheetsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Data.Entity;
 using System.Web.Http;
 using Vigilance.Models;
 using Vigilance.Models.DTOs;
@@ -21,7 +22,11 @@ namespace Vigilance.Controllers.Api
         // GET /api/usertimesheets/user_id
         public IHttpActionResult GetUserTimeSheets(string id)
         {
-            var timeSheets = _context.TimeSheets.Where(s => s.ApplicationUserId == id);
+            var timeSheets = _context.TimeSheets
+                .Include(s => s.SiteLocation)
+                .Include(s => s.LogOnLocation)
+                .Include(s => s.LogOffLocation)
+                .Where(s => s.ApplicationUserId == id).ToList();
 
             List<TimeSheetDTO> dto = new List<TimeSheetDTO>();
 
