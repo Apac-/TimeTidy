a7e5e559b6c41999570f40fef25fa34336ba2b7b
Apac <jeff@templarit.com.au>
2017-01-24 11:30:13 +1000

    Add properties

--- Vigilance/Models/DTOs/TimeSheetDTO.cs
+++ Vigilance/Models/DTOs/TimeSheetDTO.cs
@@ -1,9 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vigilance.Models.DTOs {
     public class TimeSheetDTO {
+        [Required]
+        public int WorkSiteId { get; set; }
+
+        [Required]
+        public string SiteName { get; set; }
+
+        public LatLng SiteLocation { get; set; }
+
+        public string SiteAddress { get; set; }
+
+        public DateTime LogOnTime { get; set; }
+
+        public LatLng LogOnLocation { get; set; }
     }
 }
