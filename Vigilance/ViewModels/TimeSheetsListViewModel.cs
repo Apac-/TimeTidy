b1c915d330c6aa4c3ac28b794ac48c25431beaf9
Apac <jeff@templarit.com.au>
2017-01-24 22:24:55 +1000

    Init

---
+++ Vigilance/ViewModels/TimeSheetsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vigilance.ViewModels {
+    public class TimeSheetsListViewModel {
+        public string UserId { get; set; }
+    }
+}
