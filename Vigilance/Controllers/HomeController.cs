b7bad14e46b093d62931093680924797c8edd9ad
Apac <jeff@templarit.com.au>
2017-01-24 20:10:06 +1000

    Redirect home to worksites index

--- Vigilance/Controllers/HomeController.cs
+++ Vigilance/Controllers/HomeController.cs
@@ -7,19 +7,7 @@ using System.Web.Mvc;
 namespace Vigilance.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
-        }
-
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact() {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return RedirectToAction("Index", "WorkSites");
         }
     }
 }
