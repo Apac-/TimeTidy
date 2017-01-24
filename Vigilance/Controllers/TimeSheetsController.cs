873444635e7e01e72b24883aeb4d4e72dc174a73
Apac <jeff@templarit.com.au>
2017-01-24 21:33:39 +1000

    Return view model collection

--- Vigilance/Controllers/TimeSheetsController.cs
+++ Vigilance/Controllers/TimeSheetsController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vigilance.Models;
+using Vigilance.ViewModels;
 
 namespace Vigilance.Controllers
 {
     public class TimeSheetsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public TimeSheetsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
         // GET: TimeSheets
         public ActionResult Index()
         {
@@ -16,7 +26,16 @@ namespace Vigilance.Controllers
 
         public ActionResult UserTimeSheets(string userId)
         {
+            var timeSheets = _context.TimeSheets.Where(s => s.ApplicationUserId == userId);
 
+            List<UserTimeSheetViewModel> vm = new List<UserTimeSheetViewModel>();
+
+            foreach (var timeSheet in timeSheets)
+            {
+                vm.Add(new UserTimeSheetViewModel(timeSheet));
+            }
+
+            return View(vm);
         }
     }
 }
