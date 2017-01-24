7d34b448302fce65ce2afd7e7a9b737752694b91
Apac <jeff@templarit.com.au>
2017-01-24 21:46:34 +1000

    Add fields and ctor

--- Vigilance/Models/DTOs/TimeSheetDTO.cs
+++ Vigilance/Models/DTOs/TimeSheetDTO.cs
@@ -5,5 +5,30 @@ using System.Web;
 
 namespace Vigilance.Models.DTOs {
     public class TimeSheetDTO {
+
+        public string SiteName { get; set; }
+
+        public LatLng SiteLocation { get; set; }
+
+        public string SiteAddress { get; set; }
+
+        public DateTime LogOnTime { get; set; }
+
+        public LatLng LogOnLocation { get; set; }
+
+        public DateTime? LogOffTime { get; set; }
+
+        public LatLng LogOffLocation { get; set; }
+
+        public TimeSheetDTO(TimeSheet sheet)
+        {
+            SiteName = sheet.SiteName;
+            SiteLocation = sheet.SiteLocation;
+            SiteAddress = sheet.SiteAddress;
+            LogOnTime = sheet.LogOnTime;
+            LogOnLocation = sheet.LogOnLocation;
+            LogOffTime = sheet.LogOffTime;
+            LogOffLocation = sheet.LogOffLocation;
+        }
     }
 }
