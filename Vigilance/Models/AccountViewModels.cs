959428eebb72b18369e9bf685fa5e2323a1819ea
Apac <jeff@templarit.com.au>
2017-02-10 13:34:39 +1000

    Add first and last name props

--- Vigilance/Models/AccountViewModels.cs
+++ Vigilance/Models/AccountViewModels.cs
@@ -65,6 +65,13 @@ namespace Vigilance.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
         [Phone]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
