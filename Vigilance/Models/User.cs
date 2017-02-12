4d25324c6d7862f1a46ce970b80155ed3436b73c
Apac <jeff@templarit.com.au>
2017-02-13 02:08:04 +1000

    Add required

--- Vigilance/Models/User.cs
+++ Vigilance/Models/User.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Vigilance.Models {
     public class User {
+        [Required]
         public string UserId { get; set; }
 
-        public string PhoneNumber { get; set; }
+        [Required]
         public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
 
         public List<string> UserRoles { get; set; }
