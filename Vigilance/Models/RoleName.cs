ce96dd6dd0cd062f60d8b9108ce3397a67d95896
Apac <jeff@templarit.com.au>
2017-02-13 02:41:17 +1000

    Add static role names

--- Vigilance/Models/RoleName.cs
+++ Vigilance/Models/RoleName.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Web;
 
 namespace Vigilance.Models {
-    public class RoleName
+    public static class RoleName
     {
         public const string CanManageWorkSites = "CanManageWorkSites";
+        public const string CanManageUsers = "CanManageUsers";
     }
 }
