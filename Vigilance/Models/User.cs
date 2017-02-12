afea4d693875b72925362fc248317defc25aec1e
Apac <jeff@templarit.com.au>
2017-02-13 02:19:19 +1000

    Add required to first and last name to reflect DB requirements

--- Vigilance/Models/User.cs
+++ Vigilance/Models/User.cs
@@ -12,11 +12,13 @@ namespace Vigilance.Models {
         [Required]
         public string Email { get; set; }
 
-        public string PhoneNumber { get; set; }
-
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
 
         public List<string> UserRoles { get; set; }
 
