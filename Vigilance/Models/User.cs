a6855c6b4a7fcbd7b6bf250c7274385cb0161fb5
Apac <jeff@templarit.com.au>
2017-02-13 01:04:47 +1000

    Change Id to userId

--- Vigilance/Models/User.cs
+++ Vigilance/Models/User.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace Vigilance.Models {
     public class User {
-        public string Id { get; set; }
+        public string UserId { get; set; }
 
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
