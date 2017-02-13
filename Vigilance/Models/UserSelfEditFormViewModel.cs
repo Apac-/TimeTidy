9b1b804ce4242e73f4a15eba3f64fa9e6c183efa
Apac <jeff@templarit.com.au>
2017-02-13 19:58:33 +1000

    Init

---
+++ Vigilance/Models/UserSelfEditFormViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vigilance.Models {
+    public class UserSelfEditFormViewModel
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public UserSelfEditFormViewModel(ApplicationUser user) 
+        {
+            PhoneNumber = user.PhoneNumber;
+            Email = user.Email;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+        }
+
+    }
+}
