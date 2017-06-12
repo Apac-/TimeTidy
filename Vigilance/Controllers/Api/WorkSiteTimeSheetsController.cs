7181b0a463a8fa8b7349b624e2e188f3f681dece
Apac <jeff@templarit.com.au>
2017-06-13 00:42:36 +1000

    Add work site time sheet API

---
+++ Vigilance/Controllers/Api/WorkSiteTimeSheetsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using Vigilance.Models;
+using Vigilance.Models.DTOs;
+
+namespace Vigilance.Controllers.Api
+{
+    [Authorize(Roles = RoleName.CanManageWorkSites)]
+    public class WorkSiteTimeSheetsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public WorkSiteTimeSheetsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/worksitetimesheets/1
+        public IHttpActionResult GetWorkSiteTimeSheets(int id)
+        {
+            var timeSheets = _context.TimeSheets
+                .Include(s => s.SiteLocation)
+                .Include(s => s.LogOnLocation)
+                .Include(s => s.LogOffLocation)
+                .Where(s => s.WorkSiteId == id).ToList();
+
+            List<WorkSiteTimeSheetDTO> dto = new List<WorkSiteTimeSheetDTO>();
+
+            foreach (var sheet in timeSheets)
+            {
+                dto.Add(new WorkSiteTimeSheetDTO(sheet));
+            }
+
+            return Ok(dto);
+        }
+    }
+}
