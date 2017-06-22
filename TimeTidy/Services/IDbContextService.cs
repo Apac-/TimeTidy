d0c5dd81c22723d115acf697551a2e8622f7edff
Apac <jeff@templarit.com.au>
2017-06-22 13:45:49 +1000

    Create Find User/WorkSite

--- TimeTidy/Services/IDbContextService.cs
+++ TimeTidy/Services/IDbContextService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTidy.Models;
 
 namespace TimeTidy.Services
 {
-    interface IDbContextService
+    public interface IDbContextService
     {
+        ApplicationUser FindUser(string id);
+        WorkSite FindWorkSite(int id);
     }
 }
