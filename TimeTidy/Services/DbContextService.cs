f00fe52dd94ab7985e25748d351f6523f00ac707
Apac <jeff@templarit.com.au>
2017-06-22 13:50:18 +1000

    Fill out interface requirements

--- TimeTidy/Services/DbContextService.cs
+++ TimeTidy/Services/DbContextService.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TimeTidy.Models;
 
 namespace TimeTidy.Services
 {
     public class DbContextService : IDbContextService
     {
+        private ApplicationDbContext _context;
+
+        public DbContextService()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ApplicationUser FindUser(string id)
+        {
+            return _context.Users.SingleOrDefault(u => u.Id == id);
+        }
+
+        public WorkSite FindWorkSite(int id)
+        {
+            return _context.WorkSites.SingleOrDefault(w => w.Id == id);
+        }
     }
 }
