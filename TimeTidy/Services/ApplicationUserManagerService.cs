ceae5b823e42ad3e7cc6ed110ec5a43055f70eca
Apac <jeff@templarit.com.au>
2017-06-22 18:45:12 +1000

    Implement interface

--- TimeTidy/Services/ApplicationUserManagerService.cs
+++ TimeTidy/Services/ApplicationUserManagerService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNet.Identity;
+using TimeTidy.Models;
 
 namespace TimeTidy.Services
 {
@@ -12,6 +15,21 @@ namespace TimeTidy.Services
         public ApplicationUserManagerService(ApplicationUserManager applicationUserManager)
         {
             _userManager = applicationUserManager;
+        }
+
+        public Task<IdentityResult> AddUserToRolesAsync(string userId, params string[] roles)
+        {
+            return _userManager.AddToRolesAsync(userId, roles);
+        }
+
+        public Task<ApplicationUser> FindUserByIdAsync(string userId)
+        {
+            return _userManager.FindByIdAsync(userId);
+        }
+
+        public Task<IdentityResult> RemoveUserFromRolesAsync(string userId, params string[] roles)
+        {
+            return _userManager.RemoveFromRolesAsync(userId, roles);
         }
     }
 }
