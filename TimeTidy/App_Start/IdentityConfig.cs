4267b768b2b1196dcfa109bdc127204bd9d74eae
Apac <jeff@templarit.com.au>
2017-06-22 16:54:01 +1000

    Inject dataProtectionProvider

--- TimeTidy/App_Start/IdentityConfig.cs
+++ TimeTidy/App_Start/IdentityConfig.cs
@@ -36,7 +36,7 @@ namespace TimeTidy
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
-        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+        public ApplicationUserManager(IUserStore<ApplicationUser> store, IDataProtectionProvider dataProtectionProvider)
             : base(store)
         {
             // Configure validation logic for usernames
@@ -75,7 +75,6 @@ namespace TimeTidy
             this.EmailService = new EmailService();
             this.SmsService = new SmsService();
 
-            var dataProtectionProvider = Startup.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
                 IDataProtector dataProtector = dataProtectionProvider.Create("ASP.NET Identity");
