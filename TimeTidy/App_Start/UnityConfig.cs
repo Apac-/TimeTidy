3db27cfdc24723362898a30c48ad3bbc7883ce65
Apac <jeff@templarit.com.au>
2017-06-22 14:58:05 +1000

    Register IUserStore

--- TimeTidy/App_Start/UnityConfig.cs
+++ TimeTidy/App_Start/UnityConfig.cs
@@ -3,6 +3,9 @@ using Microsoft.Practices.Unity;
 using Unity.Mvc5;
 using TimeTidy.Services;
 using TimeTidy.Controllers;
+using Microsoft.AspNet.Identity;
+using TimeTidy.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace TimeTidy
 {
@@ -17,6 +20,9 @@ namespace TimeTidy
 
             // e.g. container.RegisterType<ITestService, TestService>();
             container.RegisterType<IDbContextService, DbContextService>();
+
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(
+                new InjectionConstructor(typeof(ApplicationDbContext)));
 
             container.RegisterType<AccountController>(new InjectionConstructor());
             
