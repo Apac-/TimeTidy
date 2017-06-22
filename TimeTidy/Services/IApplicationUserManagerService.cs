c89e07da6103f3765b53c6f63d45e2e049fa5697
Apac <jeff@templarit.com.au>
2017-06-22 18:42:43 +1000

    Add find, remove, add

--- TimeTidy/Services/IApplicationUserManagerService.cs
+++ TimeTidy/Services/IApplicationUserManagerService.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTidy.Models;
 
 namespace TimeTidy.Services
 {
     interface IApplicationUserManagerService
     {
+        Task<ApplicationUser> FindUserByIdAsync(string userId);
+        Task<IdentityResult> RemoveUserFromRolesAsync(string userId, params string[] roles);
+        Task<IdentityResult> AddUserToRolesAsync(string userId, params string[] roles);
     }
 }
