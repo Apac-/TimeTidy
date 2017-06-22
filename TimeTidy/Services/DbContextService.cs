6e707e9e0a84f415455415eaf296bf4910c8aed7
Apac <jeff@templarit.com.au>
2017-06-22 19:00:37 +1000

    Add interface requirements

--- TimeTidy/Services/DbContextService.cs
+++ TimeTidy/Services/DbContextService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 using TimeTidy.Models;
 
 namespace TimeTidy.Services
@@ -17,12 +18,32 @@ namespace TimeTidy.Services
 
         public ApplicationUser FindUser(string id)
         {
+            return _context.Users.Single(u => u.Id == id);
+        }
+
+        public ApplicationUser FindUserOrDefault(string id)
+        {
             return _context.Users.SingleOrDefault(u => u.Id == id);
         }
 
         public WorkSite FindWorkSite(int id)
         {
             return _context.WorkSites.SingleOrDefault(w => w.Id == id);
+        }
+
+        public List<IdentityRole> Roles()
+        {
+            return _context.Roles.ToList();
+        }
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+
+        public List<ApplicationUser> Users()
+        {
+            return _context.Users.ToList();
         }
     }
 }
