83e63a77f5980452b17832141785041db7cc5362
Apac <jeff@templarit.com.au>
2017-06-22 16:36:18 +1000

    Install Unity.MVC bootstrapper

--- TimeTidy/App_Start/UnityConfig.cs
+++ TimeTidy/App_Start/UnityConfig.cs
@@ -1,34 +1,42 @@
-using System.Web.Mvc;
+using System;
 using Microsoft.Practices.Unity;
-using Unity.Mvc5;
-using TimeTidy.Services;
-using TimeTidy.Controllers;
-using Microsoft.AspNet.Identity;
-using TimeTidy.Models;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.Owin.Security;
-using System.Web;
+using Microsoft.Practices.Unity.Configuration;
 
-namespace TimeTidy
+namespace TimeTidy.App_Start
 {
-    public static class UnityConfig
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
+    public class UnityConfig
     {
-        public static void RegisterComponents()
+        #region Unity Container
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
         {
-			var container = new UnityContainer();
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
 
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
+        /// <summary>
+        /// Gets the configured Unity container.
+        /// </summary>
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+        #endregion
 
-            // e.g. container.RegisterType<ITestService, TestService>();
-            container.RegisterType<IDbContextService, DbContextService>();
+        /// <summary>Registers the type mappings with the Unity container.</summary>
+        /// <param name="container">The unity container to configure.</param>
+        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to 
+        /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
+            // container.LoadConfiguration();
 
-            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(
-                new InjectionConstructor(typeof(ApplicationDbContext)));
-            container.RegisterType<IAuthenticationManager>(
-                new InjectionFactory(c => HttpContext.Current.GetOwinContext().Authentication));
-            
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+            // TODO: Register your types here
+            // container.RegisterType<IProductRepository, ProductRepository>();
         }
     }
 }
