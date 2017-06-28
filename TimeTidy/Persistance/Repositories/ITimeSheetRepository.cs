85c11a46ea251aa62aa8e1f970ebb1abf5ef065d
Apac <jeff@templarit.com.au>
2017-06-28 15:52:30 +1000

    Create TimeSheetRepo

---
+++ TimeTidy/Persistance/Repositories/ITimeSheetRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TimeTidy.Models;
+
+namespace TimeTidy.Persistance.Repositories
+{
+    public interface ITimeSheetRepository
+    {
+        TimeSheet Add(TimeSheet timeSheet);
+
+        TimeSheet GetTimeSheet(int id);
+
+        IEnumerable<TimeSheet> GetTimeSheets();
+
+        TimeSheet Remove(TimeSheet timeSheet);
+    }
+}
---
+++ TimeTidy/Persistance/Repositories/TimeSheetRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeTidy.Models;
+
+namespace TimeTidy.Persistance.Repositories
+{
+    public class TimeSheetRepository : ITimeSheetRepository
+    {
+        private readonly IApplicationDbContext _context;
+
+        public TimeSheetRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public TimeSheet GetTimeSheet(int id)
+        {
+            return _context.TimeSheets.SingleOrDefault(t => t.Id == id);
+        }
+
+        public IEnumerable<TimeSheet> GetTimeSheets()
+        {
+            return _context.TimeSheets.ToList();
+        }
+
+        public TimeSheet Add(TimeSheet timeSheet)
+        {
+            return _context.TimeSheets.Add(timeSheet);
+        }
+
+        public TimeSheet Remove(TimeSheet timeSheet)
+        {
+            return _context.TimeSheets.Remove(timeSheet);
+        }
+    }
+}
