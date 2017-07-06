a2ae8a38fdee5a9f26fe3d265706c295562b2660
Apac <jeff@templarit.com.au>
2017-07-06 15:26:29 +1000

    Override equals

--- TimeTidy/Models/DTOs/LogOnTimeDTO.cs
+++ TimeTidy/Models/DTOs/LogOnTimeDTO.cs
@@ -6,6 +6,20 @@ using System.Web;
 namespace TimeTidy.Models.DTOs {
     public class LogOnTimeDTO {
         public DateTime? DateTime { get; set; }
+
         public int? TimeSheetId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            LogOnTimeDTO dto = obj as LogOnTimeDTO;
+
+            if (obj == null)
+                return false;
+
+            if (dto.DateTime == this.DateTime && dto.TimeSheetId == this.TimeSheetId)
+                return true;
+            else
+                return false;
+        }
     }
 }
