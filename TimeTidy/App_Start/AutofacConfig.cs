6ead91c6797babebd737a9962d7c7dd29238e8f1
Apac <jeff@templarit.com.au>
2017-07-07 22:53:25 +1000

    Register automapper and comments

--- TimeTidy/App_Start/AutofacConfig.cs
+++ TimeTidy/App_Start/AutofacConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
+using AutoMapper;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
@@ -24,27 +25,45 @@ namespace TimeTidy.App_Start
         {
             var builder = new ContainerBuilder();
 
+            // Context
             builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerLifetimeScope();
 
+            // User
             builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerLifetimeScope();
             builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerLifetimeScope();
             builder.Register(c => new UserStore<ApplicationUser>(c.Resolve<ApplicationDbContext>())).AsImplementedInterfaces().InstancePerLifetimeScope();
 
-            builder.RegisterType<ApplicationUserManagerService>().As<IApplicationUserManagerService>().InstancePerLifetimeScope();
+            // Unit of work
             builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerLifetimeScope();
 
+            // Automapper
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AssignableTo(typeof(Profile)).As<Profile>();
+            builder.Register(c => new MapperConfiguration(cfg =>
+            {
+                foreach (var profile in c.Resolve<IEnumerable<Profile>>())
+                {
+                    cfg.AddProfile(profile);
+                }
+            })).AsSelf().SingleInstance();
+
+            builder.Register(c => c.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>();
+
+            // Identity and authentication
             builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).As<IAuthenticationManager>();
             builder.Register(c => new IdentityFactoryOptions<ApplicationUserManager>
             {
                 DataProtectionProvider = new Microsoft.Owin.Security.DataProtection.DpapiDataProtectionProvider("TimeTidy")
             });
 
+            // Autofac filter provider
             builder.RegisterModule(new AutofacWebTypesModule());
             builder.RegisterFilterProvider();
 
+            // Controllers
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
             builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
 
+            // Finish
             IContainer container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
             GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container);
