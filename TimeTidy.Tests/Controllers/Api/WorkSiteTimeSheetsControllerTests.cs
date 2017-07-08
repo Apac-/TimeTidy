b079f370d8236fbcbcf1039ad75e2940a0b611a8
Apac <jeff@templarit.com.au>
2017-07-08 21:56:17 +1000

    Init test

---
+++ TimeTidy.Tests/Controllers/Api/WorkSiteTimeSheetsControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimeTidy.Tests.Controllers.Api
+{
+    [TestClass]
+    public class WorkSiteTimeSheetsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
