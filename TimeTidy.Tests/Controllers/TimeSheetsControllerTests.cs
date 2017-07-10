8dbc8c09a7025cd84078c366f81d6126c0348568
Apac <jeff@templarit.com.au>
2017-07-10 15:28:23 +1000

    Init test

---
+++ TimeTidy.Tests/Controllers/TimeSheetsControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using TimeTidy.Controllers;
+using TimeTidy.Persistence;
+using TimeTidy.Tests.Extensions;
+
+namespace TimeTidy.Tests.Controllers
+{
+    [TestFixture]
+    public class TimeSheetsControllerTests
+    {
+        private TimeSheetsController _controller;
+        private string _userId;
+
+        [SetUp]
+        public void Setup()
+        {
+            var mockUoW = new Mock<IUnitOfWork>();
+
+            _userId = "1";
+
+            _controller = new TimeSheetsController(mockUoW.Object);
+            _controller.MockCurrentUser(_userId, "user@domain");
+        }
+    }
+}
