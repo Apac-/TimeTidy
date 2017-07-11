208c3d93168e1f6e0bee7f1df89e7cce69d44848
Apac <jeff@templarit.com.au>
2017-07-11 14:45:17 +1000

    Init controller tests for WorkSites

---
+++ TimeTidy.Tests/Controllers/WorkSitesControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TimeTidy.Tests.Controllers
+{
+    [TestFixture]
+    public class WorkSitesControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
