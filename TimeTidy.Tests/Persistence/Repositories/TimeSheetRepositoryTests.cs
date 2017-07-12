c7c3b463c00c9fab7fb651114833d5428b97272c
Apac <jeff@templarit.com.au>
2017-07-12 17:50:19 +1000

    Init tests

---
+++ TimeTidy.Tests/Persistence/Repositories/TimeSheetRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Data.Entity;
+using TimeTidy.Models;
+using TimeTidy.Persistence;
+using TimeTidy.Persistence.Repositories;
+
+namespace TimeTidy.Tests.Persistence.Repositories
+{
+	[TestFixture]
+	public class TimeSheetRepositoryTests
+	{
+        private Mock<DbSet<TimeSheet>> _mockTimeSheets;
+        private Mock<TimeSheetRepository> _repository;
+
+        [SetUp]
+		public void Setup()
+		{
+            _mockTimeSheets = new Mock<DbSet<TimeSheet>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.TimeSheets).Returns(_mockTimeSheets.Object);
+
+            _repository = new Mock<TimeSheetRepository>(mockContext.Object);
+		}
+	}
+}
