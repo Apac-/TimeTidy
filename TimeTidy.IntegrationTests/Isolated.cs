989dd67e8b260873c66ff299998c719daad17c7a
Apac <jeff@templarit.com.au>
2017-07-13 17:24:03 +1000

    Create Isolated

---
+++ TimeTidy.IntegrationTests/Isolated.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Interfaces;
+using System.Transactions;
+
+namespace TimeTidy.IntegrationTests
+{
+    public class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+
+        public ActionTargets Targets
+        {
+            get
+            {
+                return ActionTargets.Test;
+            }
+        }
+
+        public void BeforeTest(ITest test)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        public void AfterTest(ITest test)
+        {
+            _transactionScope.Dispose();
+        }
+
+    }
+}
