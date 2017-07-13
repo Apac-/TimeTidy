51c0274c00a9db5333ffd857a3ac29f4b71eeb1f
Apac <jeff@templarit.com.au>
2017-07-13 13:49:00 +1000

    Add tests

--- TimeTidy.Tests/Persistence/Repositories/WorkSiteRepositoryTests.cs
+++ TimeTidy.Tests/Persistence/Repositories/WorkSiteRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Data.Entity;
@@ -36,5 +37,33 @@ namespace TimeTidy.Tests.Persistence.Repositories
 
             _repository = new WorkSiteRepository(mockContext.Object);
         }
+
+        #region GetWorkSite(id)
+        [Test]
+        public void GetWorkSite_SiteIdNotFound_ShouldReturnNull()
+        {
+            var result = _repository.GetWorkSite(_siteOne.Id + 999);
+
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void GetWorkSite_SiteFound_ShouldReturnFoundSite()
+        {
+            var result = _repository.GetWorkSite(_siteOne.Id);
+
+            result.ShouldBeEquivalentTo(_siteOne);
+        }
+        #endregion
+
+        #region GetWorkSites()
+        [Test]
+        public void GetWorkSites_ValidRequest_ShouldReturnAllSites()
+        {
+            var result = _repository.GetWorkSites();
+
+            result.ShouldBeEquivalentTo(new[] { _siteOne }); 
+        }
+        #endregion
     }
 }
