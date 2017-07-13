9bd5451de787ddd4ff6c57fa3ec17341a4513f73
Apac <jeff@templarit.com.au>
2017-07-13 16:44:05 +1000

    Init

---
+++ TimeTidy.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using System.Data.Entity.Migrations;
+
+namespace TimeTidy.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetUp
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            MigrateDbToLatestVersion();
+        }
+
+        private static void MigrateDbToLatestVersion()
+        {
+            var config = new TimeTidy.Migrations.Configuration();
+            var migrator = new DbMigrator(config);
+        }
+    }
+}
