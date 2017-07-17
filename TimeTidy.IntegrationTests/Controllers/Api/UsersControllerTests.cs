d1da32f5d7e75ed3073041b23ce4cc48ba564d84
Apac <jeff@templarit.com.au>
2017-07-17 15:24:33 +1000

    Init

---
+++ TimeTidy.IntegrationTests/Controllers/Api/UsersControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TimeTidy.IntegrationTests.Controllers.Api
+{
+    [TestFixture]
+    public class UsersControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
