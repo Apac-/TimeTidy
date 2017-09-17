678cdc72af5aec5830e716e378f2acd95073a9c6
Apac <jeff@templarit.com.au>
2017-09-17 14:47:56 +1000

    Rename and separate services

---
+++ TimeTidy/Scripts/App/mapBoxService.js
@@ -0,0 +1,15 @@
+﻿function setUpMap() {
+    var siteMap = L.map("mapid");
+    L
+        .tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=ENTER_MAPBOX_ACCESS_KEY_HERE",
+        {
+            attribution:
+                'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox.streets"
+        }).addTo(siteMap);
+
+    return siteMap;
+}
