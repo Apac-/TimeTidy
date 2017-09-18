419e83d8b76b7361a17d467e33bac8d6e49480e8
Apac <jeff@templarit.com.au>
2017-09-18 17:07:33 +1000

    Add geoLocation service call with success and fail

--- TimeTidy/Scripts/App/worksitesIndexController.js
+++ TimeTidy/Scripts/App/worksitesIndexController.js
@@ -1,1 +1,34 @@
 ﻿
+var worksitesIndexController = function (mapboxService, geoLocationService) {
+    var siteMap;
+
+    var init = function () {
+        siteMap = mapboxService.createSiteMap();
+        geoLocationService.getCurrentPosition(success, fail);
+    };
+
+    var success = function () {
+        let output = $("#out");
+
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+
+        output.attr("userLat", latitude);
+        output.attr("userLng", longitude);
+        siteMap.setView([latitude, longitude], 13);
+    };
+
+    var fail = function (message) {
+        let output = $("#out");
+
+        if (message) { output.append(`<p>${message}</p>`) }
+        else {
+           output.append("<p>Unable to retrieve your location</p>") 
+           alert("Fail in geolocate");
+        };
+    };
+
+    return {
+        init: init
+    };
+}();
