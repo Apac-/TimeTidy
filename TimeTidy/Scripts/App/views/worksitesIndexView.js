a4659b7e0a45a22365f5a43e829f9098aee3b3e1
Apac <jeff@templarit.com.au>
2017-09-19 15:49:47 +1000

    add Datatable population

--- TimeTidy/Scripts/App/views/worksitesIndexView.js
+++ TimeTidy/Scripts/App/views/worksitesIndexView.js
@@ -1,1 +1,23 @@
-﻿
+﻿"use strict";
+var worksitesIndexView = function () {
+    var populateSitesTable = function (jsonData) {
+        $('#sites').DataTable({
+            data: jsonData,
+            columns: [
+                {
+                    data: "name",
+                    render: function (data, type, workSite) {
+                        return "<button class='btn-link js-select' data-lat=" + workSite.lat + " data-lng=" + workSite.lng + " data-siteId=" + workSite.id + ">" + data + "</button>";
+                    }
+                },
+                {
+                    data: "streetAddress"
+                }
+            ]
+        });
+    };
+
+    return {
+        populateSitesTable: populateSitesTable,
+    };
+}();
