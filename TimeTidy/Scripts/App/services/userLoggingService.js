4ee9a09e7d482b86d269440e2934b2be86a4b38c
Apac <jeff@templarit.com.au>
2017-09-20 12:59:00 +1000

    init

---
+++ TimeTidy/Scripts/App/services/userLoggingService.js
@@ -0,0 +1,39 @@
+﻿'use strict';
+
+var UserLoggingService = function () {
+    var userLogonToSite = function (site, userLat, userLng, success, fail) {
+        let logonDto = {
+            workSiteId: site.id,
+            siteName: site.name,
+            siteLat: site.lat,
+            siteLng: site.lng,
+            siteAddress: site.streetAddress,
+            userLat: userLat,
+            userLng: userLng
+        };
+
+        $.ajax({
+            url: "/api/timeSheets",
+            method: "post",
+            data: logonDto
+        }).done(success).fail(fail);
+    };
+
+    var userLogoffOfSite = function (sheetId, userLat, userLng, success, fail) {
+        let logoffDto = {
+            userLat: userLat,
+            userLng: userLng
+        };
+
+        $.ajax({
+            url: "/api/timeSheets/" + sheetId,
+            method: "put",
+            data: logoffDto
+        }).done(success).fail(fail);
+    };
+
+    return {
+        userLogonToSite: userLogonToSite,
+        userLogoffOfSite: userLogoffOfSite,
+    };
+}();
