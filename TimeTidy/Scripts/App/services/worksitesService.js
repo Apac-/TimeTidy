1cad8286408707b068a07e5eb81f168217b298ff
Apac <jeff@templarit.com.au>
2017-09-21 17:10:32 +1000

    Add get work sites from ajax call

--- TimeTidy/Scripts/App/services/worksitesService.js
+++ TimeTidy/Scripts/App/services/worksitesService.js
@@ -1,1 +1,12 @@
 ﻿"use strict";
+
+var WorksitesService = function () {
+    var getWorksites = function (done, fail) {
+        $.getJSON("/api/worksites", { get_param: 'value' })
+            .done(done(data)).fail(fail);
+    };
+
+    return {
+        getWorksites: getWorksites,
+    };
+}();
