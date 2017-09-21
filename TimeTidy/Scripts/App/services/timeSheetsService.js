57bf5a1e8544017cc8689583ec1896135086f46e
Apac <jeff@templarit.com.au>
2017-09-21 16:49:11 +1000

    Create getter for timesheet by id

--- TimeTidy/Scripts/App/services/timeSheetsService.js
+++ TimeTidy/Scripts/App/services/timeSheetsService.js
@@ -1,1 +1,12 @@
 ﻿"use strict";
+
+var TimeSheetsService = function () {
+    var getTimeSheet = function (sheetId, done, fail) {
+        $.getJSON("/api/timesheets/" + siteId)
+            .done(done(data)).fail(fail);
+    };
+
+    return {
+        getTimeSheet: getTimeSheet,
+    };
+}();
