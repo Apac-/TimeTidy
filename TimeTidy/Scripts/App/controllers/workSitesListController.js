c8bed2cbb156a4ce268af20767159c226945d440
Apac <jeff@templarit.com.au>
2017-09-21 17:20:41 +1000

    Rename

--- TimeTidy/Scripts/App/controllers/workSitesListController.js
+++ TimeTidy/Scripts/App/controllers/workSitesListController.js
@@ -1,1 +1,34 @@
 ﻿"use strict";
+
+var WorksitesListController = function (mapboxService) {
+    let siteMap;
+
+    let mapLayerGroup;
+
+    let table = null;
+
+    var init = function () {
+        siteMap = mapboxService.createSiteMap('mapid');
+
+        mapLayerGroup = L.layerGroup().addTo(siteMap);
+
+        loadSites();
+    };
+
+    var loadSites = function () {
+
+    };
+
+    var success = function (data) {
+
+    };
+
+    var fail = function () {
+
+    };
+
+    return {
+        init: init,
+    };
+
+}(MapboxService);
