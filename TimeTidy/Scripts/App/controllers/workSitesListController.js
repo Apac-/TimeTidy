7d72b7d2560e4618c5a790697605b1f6d8b1d835
Apac <jeff@templarit.com.au>
2017-09-22 12:50:29 +1000

    Finish successful return from getting worksites

--- TimeTidy/Scripts/App/controllers/workSitesListController.js
+++ TimeTidy/Scripts/App/controllers/workSitesListController.js
@@ -1,6 +1,6 @@
 ﻿"use strict";
 
-var WorksitesListController = function (mapboxService) {
+var WorksitesListController = function (mapboxService, worksitesService, geoLocationService, worksitesListView) {
     let siteMap;
 
     let mapLayerGroup;
@@ -12,23 +12,38 @@ var WorksitesListController = function (mapboxService) {
 
         mapLayerGroup = L.layerGroup().addTo(siteMap);
 
-        loadSites();
-    };
-
-    var loadSites = function () {
-
+        worksitesService.getWorksites(sucess, fail);
     };
 
     var success = function (data) {
+        if (data.length != 0) {
+            mapboxService.setMapView(siteMap, data[0].lat, data[0].lng);
+        } else {
+            geoLocationService.getCurrentPosition(function (position) {
+                mapboxService.setMapView(siteMap, position.coords.latitude, position.coords.longitude);
+            });
+        };
 
+        table = worksitesListView.populateWorksitesTable(data);
+
+        addSiteMarkersToMap(data);
+    };
+
+    var addSiteMarkersToMap = function (data) {
+        $.each(data, function (index, element) {
+                mapboxService.addMarkerToMap(mapLayerGroup,
+                                             element.lat, element.lng,
+                                             element.name, element.name,
+                                             `<b>${element.name}</b><br>${element.streetAddress}`)
+        });
     };
 
     var fail = function () {
-
+        worksitesListView.reportError("Failed to retrive worksites from server. Try reloading.")
     };
 
     return {
         init: init,
     };
 
-}(MapboxService);
+}(MapboxService, WorksitesService, GeoLocationService, WorksitesListView);
