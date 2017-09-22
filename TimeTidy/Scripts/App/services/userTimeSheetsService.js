475606382fa60f1852526189e9635dc0f65466e0
Apac <jeff@templarit.com.au>
2017-09-22 15:08:47 +1000

    Create user time sheets service

---
+++ TimeTidy/Scripts/App/services/userTimeSheetsService.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+var UserTimeSheetsService = function () {
+    var getUserTimeSheets = function (userId, done, fail) {
+        return $.getJSON(
+            "/api/userTimeSheets/" + userId)
+            .done(done(data))
+            .fail(fail);
+    };
+
+    var deleteUserTimeSheet = function (sheetId, success, fail) {
+        return $.ajax({
+            url: "/api/usertimesheets/" + sheetId,
+            method: "DELETE",
+            success: success,
+            error: fail,
+        });
+    };
+
+
+    return {
+        getUserTimeSheets: getUserTimeSheets,
+        deleteUserTimeSheet: deleteUserTimeSheet,
+    };
+}();
