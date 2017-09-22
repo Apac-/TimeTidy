3555813804ec6e46f74be82cdf288c94f8364b5b
Apac <jeff@templarit.com.au>
2017-09-22 12:36:28 +1000

    Create site list view

---
+++ TimeTidy/Scripts/App/views/worksitesListView.js
@@ -0,0 +1,38 @@
+﻿"use strict";
+
+var WorksitesListView = function () {
+    var populateWorksitesTable = function (data) {
+        let table = $('#sites').DataTable({
+            data: data,
+            columns: [
+                {
+                    data: "name",
+                    render: function(data, type, worksite) {
+                        return "<a href='/worksites/edit/" + worksite.id + "'>" + worksite.name + "</a>";
+                    }
+                },
+                {
+                    data: "streetAddress"
+                },
+                {
+                    data: "id",
+                    render: function(data, type, worksite) {
+                        return "<button class='btn-link js-delete' data-worksite-id=" + data 
+                            + " data-worksite-name='" + worksite.name + "'>Delete</button>";
+                    }
+                }
+            ]
+        });
+
+        return table;
+    };
+
+    var reportError = function (message) {
+        alert(message);
+    };
+
+    return {
+        populateWorksitesTable: populateWorksitesTable,
+        error: error,
+    };
+}();
