6beabf56438e70dc74ac167914a245aba5d3ba09
Apac <jeff@templarit.com.au>
2017-09-22 14:16:16 +1000

    Create usersService

---
+++ TimeTidy/Scripts/App/services/usersService.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+var UsersService = function () {
+
+    var getUsers = function (done, fail) {
+        return $.getJSON(
+                    "/api/users")
+                    .done(done(data))
+                    .fail(fail);
+    };
+
+    var deleteUser = function (userId, success, fail) {
+        $.ajax({
+            url: "/api/users/" + userId,
+            method: "DELETE",
+            success: success,
+            error: fail
+        });
+    };
+
+    return {
+        getUsers: getUsers,
+        deleteUser: deleteUser,
+    };
+}();
